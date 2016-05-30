728a3145b3155220dcfe93beb4f8b173c8bed189
William Di Luigi <williamdiluigi@gmail.com>
2016-05-31 01:20:51 +0200

    Add MANIFEST.in, setup.py, cmscommon.eventsource

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='rws',
+      version='0.1.1',
+      description='Ranking Web Server',
+      author='Algorithm Ninja',
+      author_email='algorithm@ninja',
+      license='AGPL3',
+      url='https://github.com/algorithm-ninja/rws',
+      packages=find_packages(),
+      package_data={
+          '': ['*.svg', '*.js', '*.html', '*.css', '*.png', '*.ico']
+      },
+      install_requires=[
+          'six',
+          'gevent',
+          'werkzeug'
+      ],
+      entry_points={
+          'console_scripts': [
+              'rws=cmsranking.RankingWebServer:main'
+          ]
+      })
