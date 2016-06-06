653196fdb2c2ca7185917cf3254f573f15ec84bf
William Di Luigi <williamdiluigi@gmail.com>
2016-06-06 12:58:39 +0200

    Some small improvements to RWS

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rws',
-      version='0.3.0',
+      version='0.4.0',
       description='Ranking Web Server',
       author='Algorithm Ninja',
       author_email='algorithm@ninja',
